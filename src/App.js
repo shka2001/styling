81bf8022920d97beb8f5796c63f05b69f4774230
Shiho Kanno <shihopoyopoyochan@gmail.com>
2020-12-09 22:25:06 +0100

    sass

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ButtonCSS from "./components/ButtonCSS";
+import ButtonSCSS from "./components/ButtonSCSS";
 
 function App() {
   return (
     <div>
       <button>Simple Button</button>
       <ButtonCSS />
+      <ButtonSCSS />
       <button>Hej</button>
     </div>
   );
---
+++ src/components/ButtonSCSS.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "../styles/button.scss"
+export default function ButtonSCSS() {
+    return (
+        <button>
+            Button SCSS
+        </button>
+    )
+}
